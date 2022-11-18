e2671c8f85960ed0f7877da3e33d6304b84bf27e
TylerFujikawa <113073735+TylerFujikawa@users.noreply.github.com>
2022-11-18 12:44:29 -0700

    Update wires.js

--- wires.js
+++ wires.js
@@ -1,26 +1,101 @@
 //Wires
-let shapeSizeRadius = 15
-let locs = []
+let shapeSizeDiameter = 15
+let pairs = []
+let startPos
+let endPos
+let activeWire = 0
+let activeTimer = false
+let timer = 0
 
 function wiresExercise(){
+  //setup the locations of circles
+  exerciseChoice = 3
   noStroke()
-  fill('red')
-  locs = [getRandX(),getRandY(),getRandX(),getRandY(),getRandX(),getRandY(),getRandX(),getRandY(),getRandX(),getRandY(),getRandX(),getRandY(),]
-  circle(locs[0],locs[1],shapeSizeRadius)
-  circle(locs[2],locs[3],shapeSizeRadius)
-  fill('green')
-  circle(locs[4],locs[5],shapeSizeRadius)
-  circle(locs[6],locs[7],shapeSizeRadius)
-  fill('blue')
-  circle(locs[8],locs[9],shapeSizeRadius)
-  circle(locs[10],locs[11],shapeSizeRadius)
+  pairs.push(new Pair('red', 1))
+  pairs.push(new Pair('green', 2))
+  pairs.push(new Pair('blue', 3))
+  activeTimer = true
+}
+
+function mousePressed(){
+  //if mouse location is less than 15 above or below and
+  //less than 15 left or right, then it
+  if(exerciseChoice == 3){
+    mousePos = new Vector(mouseX,mouseY)
+    for(let i = 0; i < pairs.length;i++){
+      if(pairs[i].checkValid(mousePos)){
+        break
+      }
+    }
+  }
+}
   
+function checkFinished(){
+  for(let i = 0; i < pairs.length;i++){
+    if(!pairs[i].done){
+      return
+    }
+  }
+  activeTimer = false
+  wiresData.push(timer)
+}
+
+class Pair{
+  constructor(colorChoice, id, firstCircle, secondCircle, active, c1, c2){
+    this.done = false
+    this.id = id
+    this.firstCircle = new Vector(getRandX(),getRandY())
+    this.secondCircle = new Vector(getRandX(),getRandY())
+    this.colorChoice = colorChoice
+    fill(this.colorChoice)
+    this.c1 = circle(this.firstCircle.x,this.firstCircle.y,shapeSizeDiameter)
+    this.c2 = circle(this.secondCircle.x,this.secondCircle.y,shapeSizeDiameter)
+    this.active = 0 //0 is none, 1 is first, 2 is second
+  }
+  checkValid(mousePos){
+    if(activeWire == 0 || activeWire == this.id){
+      var distFirst = this.getDist(mousePos, this.firstCircle)
+      var distSecond = this.getDist(mousePos, this.secondCircle)
+      if(distFirst < shapeSizeDiameter / 2){
+        if(this.active == 0){
+          this.active = 1
+          startPos = mousePos
+          activeWire = this.id;
+          return true
+        } else if(this.active == 2){
+          push()
+          stroke(this.colorChoice)
+          line(this.firstCircle.x, this.firstCircle.y, this.secondCircle.x, this.secondCircle.y)
+          pop()
+          activeWire = 0;
+          this.done = true
+          checkFinished()
+          return true
+        }
+      }else if(distSecond < shapeSizeDiameter / 2){
+        if(this.active == 0){
+          this.active = 2
+          startPos = mousePos
+          activeWire = this.id;
+          return true
+        }else if(this.active == 1){
+          push()
+          stroke(this.colorChoice)
+          line(this.firstCircle.x, this.firstCircle.y, this.secondCircle.x, this.secondCircle.y)
+          pop()
+          activeWire = 0;
+          this.done = true
+          checkFinished()
+          return true
+        }
+      }
+    }
+    return false
+  }
+  getDist(vector1, vector2){
+    var x = vector1.x - vector2.x
+    var y = vector1.y - vector2.y
+    return sqrt(pow(x, 2) + pow(y, 2));
+  }
   
-  //3 shapes x2 spawned randomly on page
-  //shapes have an invis button over it
-  //timer when started
-  //when one shape is clicked, is starts drawing a line from that shape. (x,y)
-  //have to click on other shape
-  //on start, spawn the shapes
-  //on update, draw the lines
 }
