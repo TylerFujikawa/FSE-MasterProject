6f983b09ba748de146481c1024bf234a58e3d5cf
TylerFujikawa <113073735+TylerFujikawa@users.noreply.github.com>
2022-11-21 12:32:01 -0700

    Update wires.js

--- wires.js
+++ wires.js
@@ -8,23 +8,13 @@ let activeTimer = false
 let timer = 0
 
 function wiresExercise(){
+  //setup the locations of circles
   exerciseChoice = 3
   noStroke()
   pairs.push(new Pair('red', 1))
   pairs.push(new Pair('green', 2))
   pairs.push(new Pair('blue', 3))
   activeTimer = true
-}
-
-function mousePressed(){
-  if(exerciseChoice == 3){
-    mousePos = new Vector(mouseX,mouseY)
-    for(let i = 0; i < pairs.length;i++){
-      if(pairs[i].checkValid(mousePos)){
-        break
-      }
-    }
-  }
 }
   
 function checkFinished(){
@@ -33,8 +23,8 @@ function checkFinished(){
       return
     }
   }
-  activeTimer = false
   wiresData.push(timer)
+  resetTimer()
 }
 
 class Pair{
@@ -49,50 +39,42 @@ class Pair{
     this.c2 = circle(this.secondCircle.x,this.secondCircle.y,shapeSizeDiameter)
     this.active = 0 //0 is none, 1 is first, 2 is second
   }
-  checkValid(mousePos){
+  checkValid(){
     if(activeWire == 0 || activeWire == this.id){
-      var distFirst = this.getDist(mousePos, this.firstCircle)
-      var distSecond = this.getDist(mousePos, this.secondCircle)
-      if(distFirst < shapeSizeDiameter / 2){
+      var distFirst = getDist(mousePos, this.firstCircle)
+      var distSecond = getDist(mousePos, this.secondCircle)
+      if(distFirst <= shapeSizeDiameter / 2){
         if(this.active == 0){
           this.active = 1
           startPos = mousePos
           activeWire = this.id;
           return true
         } else if(this.active == 2){
-          push()
-          stroke(this.colorChoice)
-          line(this.firstCircle.x, this.firstCircle.y, this.secondCircle.x, this.secondCircle.y)
-          pop()
-          activeWire = 0;
-          this.done = true
-          checkFinished()
+          this.endLine()
           return true
         }
-      }else if(distSecond < shapeSizeDiameter / 2){
+      }else if(distSecond <= shapeSizeDiameter / 2){
         if(this.active == 0){
           this.active = 2
           startPos = mousePos
           activeWire = this.id;
           return true
         }else if(this.active == 1){
-          push()
-          stroke(this.colorChoice)
-          line(this.firstCircle.x, this.firstCircle.y, this.secondCircle.x, this.secondCircle.y)
-          pop()
-          activeWire = 0;
-          this.done = true
-          checkFinished()
+          this.endLine()
           return true
         }
       }
     }
     return false
   }
-  getDist(vector1, vector2){
-    var x = vector1.x - vector2.x
-    var y = vector1.y - vector2.y
-    return sqrt(pow(x, 2) + pow(y, 2));
+  endLine(){
+    push()
+    stroke(this.colorChoice)
+    line(this.firstCircle.x, this.firstCircle.y, this.secondCircle.x, this.secondCircle.y)
+    pop()
+    activeWire = 0;
+    this.done = true
+    checkFinished()
   }
   
 }
