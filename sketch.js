32d20870ccbabd74b5dbcb62262f2c7020db9e94
TylerFujikawa <113073735+TylerFujikawa@users.noreply.github.com>
2022-11-21 12:31:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 //vars
 let mainMenu = true
+let mousePos
 
 //Data vars
-let circleData = []
+let circlesData = []
 let typingData = []
 let wiresData = []
 
@@ -35,7 +36,8 @@ function setup() {
   mainMenuButton.mousePressed(loadMainMenu).center().hide()
   dataMenuButton = createButton("Data Menu")
   dataMenuButton.mousePressed(loadDataMenu).center()
-  
+  replayButton = createButton("Replay")
+  replayButton.mousePressed(replay).center().hide()
   loadMainMenu()
 }
 function preload(){
@@ -45,18 +47,42 @@ function preload(){
   wires = "wire.png"
 }
 function draw() {
-  //Default exercise screen
-  cMousePos = new Vector(mouseX,mouseY)
-    //show the exercises
-  if(exerciseChoice == 3){
-    if(activeTimer){
-      fill("white")
-      rect(220,70,60,20)
-      fill("black")
-      textSize(16)
-      text("Time: " + timer, 250, 80)
-      if (frameCount % 60 == 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-        timer ++;
+  mousePos = new Vector(mouseX, mouseY)
+  if(exerciseChoice == 1 && drawCircle){
+    clearArea()
+    checkNextPoint()
+    fill('white')
+    stroke('red')
+    strokeWeight(1)
+    circle(cPos.x, cPos.y, circleDiameter)
+  }
+  updateTimer()
+}
+function updateTimer(){
+  if(activeTimer){
+    stroke('white')
+    fill("white")
+    rect(220,70,60,20)
+    fill("black")
+    textSize(16)
+    text("Time: " + timer, 250, 80)
+    if (frameCount % 60 == 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+      timer ++;
+    }
+  }
+}
+function resetTimer(){
+  activeTimer = false
+  timer = 0;
+}
+function mousePressed(){
+  if(exerciseChoice == 1){
+    checkClick()
+  }
+  else if(exerciseChoice == 3){
+    for(let i = 0; i < pairs.length;i++){
+      if(pairs[i].checkValid()){
+        break
       }
     }
   }
@@ -71,10 +97,30 @@ function getRandX(){
 function getRandY(){
   return floor(random(110, 390))
 }
-
+function clearArea(){
+  fill('white')
+  stroke('black')
+  rect(100, 100, 300, 300)
+}
+function replay(){
+  if(exerciseChoice == 1){
+    loadExercise1()
+  }
+  else if(exerciseChoice == 2){
+    loadExercise2()
+  }
+  else if(exerciseChoice == 3){
+    loadExercise3()
+  }
+}
 class Vector{
   constructor(x, y){
     this.x = x;
     this.y = y;
   } 
+}
+function getDist(vector1, vector2){
+  var x = vector1.x - vector2.x
+  var y = vector1.y - vector2.y
+  return sqrt(pow(x, 2) + pow(y, 2));
 }
