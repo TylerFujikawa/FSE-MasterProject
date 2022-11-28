01d23a9d71ee1ba9dc7205683fd7999840d3f679
TylerFujikawa <113073735+TylerFujikawa@users.noreply.github.com>
2022-11-28 01:07:28 -0700

    Update load.js

--- load.js
+++ load.js
@@ -1,21 +1,29 @@
-function loadExercise1(){
-  exerciseName = "Circles"
+let nameX = 0
+const nameY = 120
+function loadExercise1(){//circles
+  outline()
+  exerciseName = "Tracking Circles"
+  nameX = 230
   loadExerciseOverlay()
   circleExerciseButton.hide()
   typingExerciseButton.show()
   wiresExerciseButton.show()
   circlesExercise()
 }
-function loadExercise2(){
-  exerciseName = "Typing"
+function loadExercise2(){//typing
+  outline()
+  exerciseName = "Quick Type"
+  nameX = 290
   loadExerciseOverlay()
   circleExerciseButton.show()
   typingExerciseButton.hide()
   wiresExerciseButton.show()
   typingExercise()
 }
-function loadExercise3(){
-  exerciseName = "Wires"
+function loadExercise3(){//wires
+  outline()
+  exerciseName = "Connection"
+  nameX = 290
   loadExerciseOverlay()
   circleExerciseButton.show()
   typingExerciseButton.show()
@@ -25,84 +33,127 @@ function loadExercise3(){
 
 function loadExerciseOverlay(){
   clearCanvas()
+  outline()
   //outlines
+  push()
   strokeWeight(10)
-  rect(0,0,400,400)
-  strokeWeight(5)
+  rect(0,0,600,600)
   
   //top bar
-  rect(100,0,300,100)
-  textSize(32)
-  text(exerciseName,250,50)//exercise name text
+  strokeWeight(5)
+  rect(200,0,400,200)
+  textSize(48)
+  text(exerciseName,nameX,nameY)//exercise name text
   
   //side bar
-  rect(0,0,100,400)
+  rect(0,0,200,600)
   mainMenuButton.position(10, 50).show()
-  circleExerciseButton.position(20,125)
-  typingExerciseButton.position(20,200)
-  wiresExerciseButton.position(20,275)
-  dataMenuButton.position(10,355)
-  replayButton.position(16, 320).show()
+  circleExerciseButton.position(45,150)
+  typingExerciseButton.position(45,250)
+  wiresExerciseButton.position(45,350)
+  //dataMenuButton.position(10,355)
+  replayButton.position(55, 450).show()
+  textInput.hide()
+  enterButton.hide()
   
+  //dividers
+  push()
+  strokeWeight(3)
+  line(20, 120, 180, 120)
+  line(20, 430, 180, 430)
+  pop()
+  
+  //timer area
+  rect(0, 500, 200, 100)
+  pop()
   resetTimer()
 }
 
+//scrapped idea
 function loadDataMenu(){
+  exerciseChoice = 0
+  outline()
   clearCanvas()
   //main page
   mainMenuButton.position(165,350).show()
   circleExerciseButton.position(25,50).show()
   typingExerciseButton.position(25,150).show()
   wiresExerciseButton.position(25,250).show()
-  dataMenuButton.position(160,325).hide()
+  //dataMenuButton.position(160,325).hide()
   replayButton.hide()
+  textInput.hide()
+  enterButton.hide()
   
   exerciseChoice = 0
   
   resetTimer()
   
   //border
+  push()
   strokeWeight(5)
   rect(0,0,400,400)
   strokeWeight(5)
   line(0,100,400,100)
   line(0,200,400,200)
   line(0,300,400,300)
+  pop()
 }
 function loadMainMenu(){
+  exerciseChoice = 0
+  outline()
   clearCanvas()
   //main page
   mainMenuButton.hide()
-  circleExerciseButton.position(25,230).show()
-  typingExerciseButton.position(175,230).show()
-  wiresExerciseButton.position(325,230).show()
-  dataMenuButton.position(160,325).show()
+  circleExerciseButton.position(50,350).show()
+  typingExerciseButton.position(250,350).show()
+  wiresExerciseButton.position(450,350).show()
+  //dataMenuButton.position(160,325).show()
   replayButton.hide()
+  textInput.hide()
+  enterButton.hide()
   
   resetTimer()
   
-  //border
+  //border rect
+  push()
   strokeWeight(10)
-  rect(0,0,400,400)
+  rect(0,0,600,600)
   
   //Title
-  textSize(32)
-  textAlign(CENTER, CENTER);
-  text('Trainer',200,50)
+  textSize(64)
+  textAlign(CENTER, CENTER)
+  push()
+  fill('#05daff')
+  stroke('black')
+  strokeWeight(3)
+  text('Reaction',300,100)
+  pop()
   
   //Subtitle
-  textSize(12)
-  text('To help recovering stroke victims with their fine-motor skills',100,70,200,50)
+  textSize(16)
+  text('To help recovering stroke victims with their fine-motor skills using exercises based on speed.',150,145,300,50)
   
   //Exercises
-  textSize(24)
-  text('Exercises',200,175)
+  textSize(36)
+  text('Exercises',300,300)
   
   //Data
-  textSize(24)
-  text('Data',200,300)
+  //textSize(24)
+  //text('Data',200,300)
   
   //Footer
-  textSize(10)
-  text('Created By: Tyler Fujikawa, Yashashwi Gautom, Truong Luong, Sachin Rama',200,385)
+  textSize(16)
+  text('Created By:\nTyler Fujikawa, Yashashwi Gautom, Truong Luong, Sachin Rama',300,570)
+  pop()
+  
+  //Debug
+  //loadExercise1()
+  //loadExercise2()
+  //loadExercise3()
+}
+function outline(){
+  push()
+  strokeWeight(10)
+  rect(0,0,600,600)
+  pop()
 }
